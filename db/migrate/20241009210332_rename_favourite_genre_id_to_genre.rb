61a895f5bfb3de103569674f44c732d89b992987
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-09 18:24:09 -0300

    cleaning up

--- db/migrate/20241009210332_rename_favourite_genre_id_to_genre.rb
+++
@@ -1,5 +0,0 @@
-class RenameFavouriteGenreIdToGenre < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :directors
-  end
-end
--- db/migrate/20241009211102_drop_films_table.rb
+++
@@ -1,5 +0,0 @@
-class DropFilmsTable < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :films
-  end
-end
