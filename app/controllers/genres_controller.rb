a79d7ef3c425eccd2d63884401bbaa9687d6b79b
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-09 13:54:34 -0300

    edit

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -6,4 +6,37 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
   end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+
+    if @genre.save
+      redirect_to @genre
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+
+    if @genre.update(genre_params)
+      redirect_to @genre
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def genre_params
+      params.require(:genre).permit(:title)
+    end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ApplicationRecord
   has_many :director
   has_many :film
+
+  validates :title, presence: true
 end
