aafa272fabb1dcccf42f6b2602cb84a5947fd774
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-09 12:00:43 -0300

    Genre Read and Show + nav

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_09_040657) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_09_041220) do
   create_table "directors", force: :cascade do |t|
     t.string "name"
     t.string "nationality"
@@ -21,6 +21,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_09_040657) do
     t.index ["favourite_genre_id"], name: "index_directors_on_favourite_genre_id"
   end
 
+  create_table "films", force: :cascade do |t|
+    t.string "title"
+    t.date "release_date"
+    t.text "plot"
+    t.string "country_of_origin"
+    t.integer "duration"
+    t.integer "director_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "index_films_on_director_id"
+    t.index ["genre_id"], name: "index_films_on_genre_id"
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -28,4 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_09_040657) do
   end
 
   add_foreign_key "directors", "favourite_genres"
+  add_foreign_key "films", "directors"
+  add_foreign_key "films", "genres"
 end
